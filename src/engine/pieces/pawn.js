da3a2020ef4e2d65d2508d8eae16deac92ae23e4
Lee Doughty <leedoughtydesign@gmail.com>
2020-07-13 11:52:58 +0100

    Add logic for getAvailableMoves method in pawn

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,11 +1,16 @@
-import Piece from './piece';
+import Piece from "./piece";
+import Player from "../player";
 
 export default class Pawn extends Piece {
-    constructor(player) {
-        super(player);
-    }
+  constructor(player) {
+    super(player);
+  }
 
-    getAvailableMoves(board) {
-        return new Array(0);
+  getAvailableMoves(board) {
+    if (this.player === Player.WHITE) {
+      return new Array({ row: 1, col: 0 });
+    } else {
+      return new Array({ row: 6, col: 7 });
     }
+  }
 }
