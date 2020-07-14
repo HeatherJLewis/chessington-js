5116c85b534baf7b5ce0455da7c2408ee8e8687b
Heather Lewis <heather.lewis@bbc.co.uk>
2020-07-14 13:16:16 +0100

    Red-6: Adds moves for knight

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,11 +1,20 @@
 import Piece from './piece';
-
+import Square from '../square'
 export default class Knight extends Piece {
     constructor(player) {
         super(player);
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        const currentLocation = board.findPiece(this);
+        let arrayOfLocations = [];
+
+        const arrayOfmoves = [ [2, 1], [1, 2], [-1, 2], [-2, 1], [-2, -1], [-1, -2], [1, -2], [2, -1]];
+
+        arrayOfmoves.forEach(el => arrayOfLocations.push(Square.at(currentLocation.row + el[0], currentLocation.col + el[1])))
+
+        const arrayOfFilteredLocations = arrayOfLocations.filter(location => (location.row > -1 && location.row < 8) && (location.col > -1 && location.col < 8));
+
+        return arrayOfFilteredLocations;
     }
 }
